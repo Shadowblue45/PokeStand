d0e52b7c2046644111f512870bb40aa5770e1172
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-03-02 11:31:39 -0500

    created pokemon class

---
+++ PokeStand/src/garrettPokemonTraining/Pokemon.java
@@ -0,0 +1,16 @@
+package garrettPokemonTraining;
+
+public class Pokemon {
+	
+	private int hp;
+	private int atk;
+	private int def;
+	private int sAtk;
+	private int sDef;
+	private int spd;
+	
+	public Pokemon() {
+		
+	}
+
+}
