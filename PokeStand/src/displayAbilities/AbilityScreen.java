5a2ab838d08d705a8c2ec1d060c68370a1827abe
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-03-02 11:21:37 -0500

    stuff

--- PokeStand/src/displayAbilities/AbilityScreen.java
+++ PokeStand/src/displayAbilities/AbilityScreen.java
@@ -1,5 +1,21 @@
 package displayAbilities;
 
-public class AbilityScreen {
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class AbilityScreen extends FullFunctionScreen {
+
+	public AbilityScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- PokeStand/src/selectionScreen/SelectionScreen.java
+++ PokeStand/src/selectionScreen/SelectionScreen.java
@@ -1,5 +1,21 @@
 package selectionScreen;
 
-public class SelectionScreen {
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class SelectionScreen extends FullFunctionScreen {
+
+	public SelectionScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
