eb23c59c6f97d6208bee0ff00e46f102f5567fa2
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-03-02 11:31:02 -0500

    push

---
+++ PokeStand/src/rickyShopInteract/RickyShop.java
@@ -0,0 +1,26 @@
+package rickyShopInteract;
+
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class RickyShop extends FullFunctionScreen {
+
+	public RickyShop(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
