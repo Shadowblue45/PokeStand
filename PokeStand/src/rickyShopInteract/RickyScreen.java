ef8e13ca10c28e117db099c1cfc1e3749434cea6
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-03-20 11:30:17 -0400

    push

--- PokeStand/src/rickyShopInteract/RickyScreen.java
+++ PokeStand/src/rickyShopInteract/RickyScreen.java
@@ -20,6 +20,7 @@ public class RickyScreen extends FullFunctionScreen {
 	private ArrayList<String> itemsInShop;
 	private ArrayList<String> itemDesc;
 	private int dollars = 0;
+	private ArrayList<Integer> priceAmount;
 	
 	private static final long serialVersionUID = 5855860528658762993L;
 
@@ -35,20 +36,23 @@ public class RickyScreen extends FullFunctionScreen {
 		setPokemonFont(16);
 		itemDescriptions(viewObjects);		
 		addButtons(viewObjects);
+		setPokemonGBFont(13);
+		populatePrices();
+		addPrices(viewObjects);
 	}
 	
 	public void addItemsToArrayList() {
 		itemsInShop = new ArrayList<String>();
 		
 		itemsInShop.add("resources/ShopItems/Ether.png");
+		itemsInShop.add("resources/ShopItems/MaxEther.png");
 		itemsInShop.add("resources/ShopItems/FullHeal.png");
 		itemsInShop.add("resources/ShopItems/FullRestore.png");		
-		itemsInShop.add("resources/ShopItems/HyperPotion.png");
 		
-		itemsInShop.add("resources/ShopItems/MaxEther.png");
-		itemsInShop.add("resources/ShopItems/MaxPotion.png");
 		itemsInShop.add("resources/ShopItems/Potion.png");
 		itemsInShop.add("resources/ShopItems/SuperPotion.png");
+		itemsInShop.add("resources/ShopItems/HyperPotion.png");		
+		itemsInShop.add("resources/ShopItems/MaxPotion.png");
 	}
 	
 	public void addImagestoShop(List<Visible> viewObjects) {
@@ -57,7 +61,7 @@ public class RickyScreen extends FullFunctionScreen {
 			viewObjects.add(a);
 		}
 		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
-			Graphic b = new Graphic(665, -300 + i*150, 50, 50, itemsInShop.get(i));
+			Graphic b = new Graphic(665, -500 + i*150, 50, 50, itemsInShop.get(i));
 			viewObjects.add(b);
 		}
 		Graphic a = new Graphic(1235,40,30,30,"resources/dollar.png");
@@ -74,7 +78,7 @@ public class RickyScreen extends FullFunctionScreen {
 		}
 		
 		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
-			TextArea d = new TextArea(730, -300 + i*150, 250, 150, itemDesc.get(i));
+			TextArea d = new TextArea(730, -500 + i*150, 250, 150, itemDesc.get(i));
 			d.setCustomTextColor(Color.BLACK);
 			viewObjects.add(d);
 		}
@@ -84,16 +88,14 @@ public class RickyScreen extends FullFunctionScreen {
 		itemDesc = new ArrayList<String>();
 		
 		itemDesc.add("Restores 10 PP of a single move of a Pokémon");
+		itemDesc.add("Fully restores the PP of one of the player's selected Pokémon's move");
 		itemDesc.add("Fully heals any status problems that a Pokémon holds");
 		itemDesc.add("Fully restores the HP and heals any Status ailments of a Pokémon");
-		itemDesc.add("Heals a Pokémon by 200 HP");
-		itemDesc.add("Fully restores the PP of one of the player's selected Pokémon's move");
 		
-		itemDesc.add("If used on a fainted Pokémon, the Pokémon will be revived and have its health restored");
-		itemDesc.add("Fully restores the HP of a Pokémon");
 		itemDesc.add("Heals 20 HP of a Pokémon");
-		itemDesc.add("Revives your fainted Pokémon by half of its HP");
 		itemDesc.add("Heals 50 HP of a Pokémon");
+		itemDesc.add("Heals a Pokémon by 200 HP");		
+		itemDesc.add("Fully restores the HP of a Pokémon");
 	}
 
 	public void addBackgroundLabel(List<Visible> viewObjects) {
@@ -108,7 +110,7 @@ public class RickyScreen extends FullFunctionScreen {
 		
 		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
 			StyledComponent.setButtonOutline(true);
-			Button b = new Button(655, -300 + i*150, 565, 150, "",null);
+			Button b = new Button(655, -500 + i*150, 565, 150, "",null);
 			b.setEnabled(false);
 			b.setBackground(Color.ORANGE);
 			b.update();
@@ -134,13 +136,77 @@ public class RickyScreen extends FullFunctionScreen {
 		}
 		
 		for (int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
-			Button b = new Button(980, -325 + i*150, 75, 125,"Buy", null);
+			Button b = new Button(980, -525 + i*150, 75, 125,"Buy", null);
 			viewObjects.add(b);
 		}
 		
 		for (int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
-			Button b = new Button(1105, -325 + i*150, 75, 125,"Sell", null);
+			Button b = new Button(1105, -525 + i*150, 75, 125,"Sell", null);
 			viewObjects.add(b);
+		}
+	}
+	
+	public void populatePrices() {
+		priceAmount = new ArrayList<Integer>();
+		
+		priceAmount.add(1200);
+		priceAmount.add(2000);
+		priceAmount.add(600);
+		priceAmount.add(3000);
+		priceAmount.add(300);
+		priceAmount.add(700);
+		priceAmount.add(1200);
+		priceAmount.add(2500);
+		
+	}
+	
+	public void addPrices(List<Visible> viewObjects) {
+		for(int i = 0; i < itemsInShop.size()/2; i++) {
+			Graphic c = new Graphic(425, 100 + i*150, 15, 15, "resources/dollar.png");
+			viewObjects.add(c);
+		}
+		
+		for(int i = 0; i < itemsInShop.size()/2; i++) {
+			Graphic c = new Graphic(550, 100 + i*150, 15, 15, "resources/dollar.png");
+			viewObjects.add(c);
+		}
+		
+		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
+			Graphic c = new Graphic(1030, -500 + i*150, 15, 15, "resources/dollar.png");
+			viewObjects.add(c);
+		}
+		
+		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
+			Graphic c = new Graphic(1155, -500 + i*150, 15, 15, "resources/dollar.png");
+			viewObjects.add(c);
+		}
+		
+		for(int i = 0; i < itemsInShop.size()/2; i++) {
+			Button d = new Button(300, 50 + i*150, 200, 125, Integer.toString(priceAmount.get(i)),null);
+			d.setEnabled(false);
+			d.update();
+			viewObjects.add(d);
+		}
+		
+		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
+			Button d = new Button(910,-550 + i*150, 200, 125, Integer.toString(priceAmount.get(i)),null);
+			d.setEnabled(false);
+			d.update();
+			viewObjects.add(d);
+		}
+		
+		for(int i = 0; i < itemsInShop.size()/2; i++) {
+			Button d = new Button(425, 50 + i*150, 200, 125, Integer.toString(priceAmount.get(i)/2),null);
+			d.setEnabled(false);
+			d.update();
+			viewObjects.add(d);
+		}
+		
+		for(int i = itemsInShop.size()/2; i < itemsInShop.size(); i++) {
+			Button d = new Button(1025, -550 + i*150, 200, 125, Integer.toString(priceAmount.get(i)/2),null);
+			d.setEnabled(false);
+			d.update();
+			viewObjects.add(d);
 		}
 	}
 	
@@ -162,5 +228,15 @@ public class RickyScreen extends FullFunctionScreen {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void setPokemonGBFont(float f) {
+		try {
+			StyledComponent.setBaseFont(Font.createFont(Font.TRUETYPE_FONT, new File("resources/Pokemon GB.ttf")).deriveFont(f));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
 
