3ed3a5f8fc95d7116c62853761083d1689d720f0
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-04-11 11:31:33 -0400

    sprite stuff

--- PokeStand/src/battle/BattleScreen.java
+++ PokeStand/src/battle/BattleScreen.java
@@ -16,9 +16,9 @@ public class BattleScreen extends FullFunctionScreen{
 	public void initAllObjects(List<Visible> viewObjects) {
 		Graphic battle = new Graphic(0,0,getWidth(),getHeight(),"resources/pokebattle.jpg");
 		viewObjects.add(battle);
-		Graphic userPokemon = new Graphic(160,500-getHeight()/3,getWidth()/2,getHeight()/2,"resources/infernape back.png");
+		Graphic userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/typhlosion.png");
 		viewObjects.add(userPokemon);
-		Graphic box = new Graphic(0,500,getWidth(),500-getHeight(),"resources/Box.jpg");
+		Graphic box = new Graphic(0,500,getWidth(),500,"resources/Box.jpg");
 		viewObjects.add(box);
 	}
 
