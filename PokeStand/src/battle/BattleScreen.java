72b41283e04c5bb3b743eb49e93cdc57f78e6afe
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-04-12 10:54:52 -0400

    fixed filepath

--- PokeStand/src/battle/BattleScreen.java
+++ PokeStand/src/battle/BattleScreen.java
@@ -16,7 +16,7 @@ public class BattleScreen extends FullFunctionScreen{
 	public void initAllObjects(List<Visible> viewObjects) {
 		Graphic battle = new Graphic(0,0,getWidth(),getHeight(),"resources/pokebattle.jpg");
 		viewObjects.add(battle);
-		Graphic userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/typhlosion back.png");
+		Graphic userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/fireback/typhlosion back.png");
 		viewObjects.add(userPokemon);
 		Graphic box = new Graphic(0,500,getWidth(),500,"resources/Box.jpg");
 		viewObjects.add(box);
