02cdba427c8f18aff06c6a4f427f12271079c37e
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-04-25 11:08:40 -0400

    test

--- PokeStand/src/battle/BattleScreen.java
+++ PokeStand/src/battle/BattleScreen.java
@@ -2,21 +2,25 @@ package battle;
 
 import java.util.List;
 
+import garrettPokemonTraining.Inventory;
 import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class BattleScreen extends FullFunctionScreen{
 
+	private String name = Inventory.pokemon.getName();
+	
 	public BattleScreen(int width, int height) {
 		super(width, height);
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		System.out.println(name);
 		Graphic battle = new Graphic(0,0,getWidth(),getHeight(),"resources/pokebattle.jpg");
 		viewObjects.add(battle);
-		Graphic userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/pokebacks/charizard back.png");
+		Graphic userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/pokebacks/ "+ name +" back.png");
 		viewObjects.add(userPokemon);
 		Graphic box = new Graphic(0,500,getWidth(),500,"resources/Box.jpg");
 		viewObjects.add(box);
