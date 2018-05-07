c63ed2aad95936ed90a2eb8d7ab3e61fd44e6227
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-05-07 11:38:01 -0400

    started enemyPokemon stuff

--- PokeStand/src/battle/BattleScreen.java
+++ PokeStand/src/battle/BattleScreen.java
@@ -17,6 +17,7 @@ public class BattleScreen extends FullFunctionScreen{
 	private CustomRect currentHp;
 	private TextLabel name;
 	private Graphic userPokemon;
+	private Graphic enemyPokemonSprite;
 	private Pokemon enemyPokemon;
 	
 	public BattleScreen(int width, int height) {
@@ -26,18 +27,19 @@ public class BattleScreen extends FullFunctionScreen{
 	public void startBattle() {
 		String[] pokeNames = PokeStart.inventory.getNames();
 		int index = PokeStart.inventory.pokemonIndex;
-		if(Inventory.pokemon.getType1().equals("Fire")) {
-		enemyPokemon = new Pokemon("Suicune","Water",null,310,139,211,166,211,157,null);	
-		}
+		enemyPokemon = PokeStart.selectionScreen.getEnemyPokemon();
+		//enemyPokemon = new Pokemon("Suicune","Water",null,310,139,211,166,211,157,null);	
 		Inventory.pokemon.setCurrentHp(Inventory.pokemon.getHp());
 		int current = Inventory.pokemon.getCurrentHp();
-		int hpSize = (int) (265*current)/(Inventory.pokemon.getHp());
+		int hpSize = (int) (260*current)/(Inventory.pokemon.getHp());
 		System.out.println("\nHp Size : " + hpSize);
 		System.out.println(Inventory.pokemon.getHp());
 		System.out.println("current HP is "+Inventory.pokemon.getCurrentHp());
 		currentHp.setDimensions((int)(hpSize+1), 20);
 		name.setText(pokeNames[index]);
+		System.out.println("enemyName is "+ enemyPokemon.getName());
 		userPokemon.loadImages("resources/pokebacks/"+pokeNames[index]+ " back.png", getWidth()/2,getHeight()/2);
+		enemyPokemonSprite.loadImages("resources/pokefronts/"+ enemyPokemon.getName() + " front.png", getWidth()/2, getHeight()/2);
 		
 	}
 	
@@ -45,11 +47,13 @@ public class BattleScreen extends FullFunctionScreen{
 	public void initAllObjects(List<Visible> viewObjects) {
 		Graphic battle = new Graphic(0,0,getWidth(),getHeight(),"resources/pokebattle.jpg");
 		userPokemon = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/pokebacks/charmander back.png");
+		enemyPokemonSprite = new Graphic(820,60,getWidth()/2,getHeight()/2,"resources/pokebacks/charmander back.png");
 		Graphic box = new Graphic(0,500,getWidth(),500,"resources/Box.jpg");
-		CustomRect totalHp = new CustomRect(925,400,265,20,Color.red);
+		CustomRect totalHp = new CustomRect(925,402,260,20,Color.red);
 		name = new TextLabel(910,350,300,50,"");
-		currentHp = new CustomRect(925,400,1,20,Color.green);
+		currentHp = new CustomRect(925,402,1,20,Color.green);
 		viewObjects.add(battle);
+		viewObjects.add(enemyPokemonSprite);		
 		viewObjects.add(userPokemon);
 		viewObjects.add(box);
 		viewObjects.add(totalHp);
