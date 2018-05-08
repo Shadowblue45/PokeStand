966dd4c7867fb058aad2cd0434352eace736b681
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-05-08 11:20:09 -0400

    push

--- PokeStand/src/rickyShopInteract/RickyInteractScreen.java
+++ PokeStand/src/rickyShopInteract/RickyInteractScreen.java
@@ -19,18 +19,9 @@ public class RickyInteractScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		if(PokeStart.inventory.pokemonForms[PokeStart.inventory.pokemonIndex].equals("Charmander")) {
-			Graphic g = new Graphic(0,0,getWidth(),getHeight(),"resources/interact/Charmander.gif");
-		}
-		else if (PokeStart.inventory.pokemonForms[PokeStart.inventory.pokemonIndex].equals("Charmeleon")) {
-			
-		}
-		else if (PokeStart.inventory.pokemonForms[PokeStart.inventory.pokemonIndex].equals("Charizard")) {
-		
-		}
-		else if (PokeStart.inventory.pokemonForms[PokeStart.inventory.pokemonIndex].equals("")) {
-			
-		}
+		String name = PokeStart.inventory.pokemonForms[PokeStart.inventory.pokemonIndex];
+			Graphic g = new Graphic(0,0,getWidth(),getHeight(),"resources/interact/" + name + ".gif");
 	}
+		
 
 }
