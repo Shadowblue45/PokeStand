903ec6f40294209f63b75fb53460640efa425614
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-05-09 11:31:04 -0400

    start of heal animation

--- PokeStand/src/RestScreen/RestScreen.java
+++ PokeStand/src/RestScreen/RestScreen.java
@@ -2,11 +2,17 @@ package RestScreen;
 
 import java.util.List;
 
+import audioPlayer.AudioTest;
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class RestScreen extends FullFunctionScreen {
+
+	Graphic background;
+	int i =0;
 
 	public RestScreen(int width, int height) {
 		super(width, height);
@@ -15,8 +21,27 @@ public class RestScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		Graphic background = new Graphic(0, 0, 1280,720, "resources/heal.gif");
+	    background = new Graphic(0, 0, getWidth(),getHeight(), "resources/HealAnimation/0.jpg");
 		viewObjects.add(background);		
+		
+		
+		Button test = new Button(175,200,100,100,"Yes",new Action() {
+			
+			@Override
+			public void act() {
+				AudioTest.playSound("resources/rest.wav");
+				
+				background.loadImages("resources/HealAnimation/"+i+".jpg", getWidth(), getHeight());
+				background.update();
+				i++;
+				System.out.println(i);
+				
+			}
+		});
+		viewObjects.add(test);		
+		
+				
+		
 	}
 
 }
