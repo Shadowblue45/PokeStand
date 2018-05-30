84c4d93badfbc59e15c0b55f2a2214336fcf7a98
Jacob1531 <37192976+Jacob1531@users.noreply.github.com>
2018-05-30 15:19:37 -0400

    fixed pokemon position

--- PokeStand/src/garrettPokemonTraining/StatTrainingScreen.java
+++ PokeStand/src/garrettPokemonTraining/StatTrainingScreen.java
@@ -52,7 +52,7 @@ public class StatTrainingScreen extends FullFunctionScreen {
 		p = PokeStart.inventory.getPokemon();
 		int index = PokeStart.inventory.pokemonIndex;
 		System.out.println(pokeNames[index]);
-		poke = new Graphic(150,250,getWidth()/2,getHeight()/2,"resources/pokebacks/"+ pokeNames[index] +" back.png");
+		poke = new Graphic(150,270,getWidth()/2,getHeight()/2,"resources/pokebacks/"+ pokeNames[index] +" back.png");
 		viewObjects.add(poke);
 		Graphic box = new Graphic(0,500,getWidth(),500,"resources/Box.jpg");
 		viewObjects.add(box);
