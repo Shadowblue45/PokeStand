061b56cbfcabf6abb52b8d8338b765b77db433da
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-06-01 11:27:15 -0400

    push

--- PokeStand/src/rickyShopInteract/RickyInteractScreen.java
+++ PokeStand/src/rickyShopInteract/RickyInteractScreen.java
@@ -14,7 +14,7 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 import startGame.PokeStart;
 
 public class RickyInteractScreen extends FullFunctionScreen {
-	
+
 	private Button backButton;
 	private Button yes;
 	private Button no;
@@ -29,10 +29,12 @@ public class RickyInteractScreen extends FullFunctionScreen {
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		StyledComponent.setButtonOutline(false);
-		displayPokemon(viewObjects);
-		interact(viewObjects);
+		if(PokeStart.inventory.daysLeft >= 1) {	
+			displayPokemon(viewObjects);
+			interact(viewObjects);
+		}
 	}
-	
+
 	public void displayPokemon(List<Visible> viewObjects) {
 		Graphic background = new Graphic(0,0,getWidth(),getHeight(),"resources/interact background.png");
 		viewObjects.add(background);
@@ -42,7 +44,7 @@ public class RickyInteractScreen extends FullFunctionScreen {
 		//viewObjects.add(h);
 		viewObjects.add(g);
 	}
-	
+
 	public void interact(List<Visible> viewObjects) {
 		Graphic tb = new Graphic(440, 550, 500, 150,"resources/text box.png");
 		PokeStart.setPokemonGBFont(14f);
@@ -51,38 +53,44 @@ public class RickyInteractScreen extends FullFunctionScreen {
 			ta = new TextArea(455,575,425,150,"Would you like to \ninteract with your " +pokemon + "?");
 		}
 		else {
-			PokeStart.setPokemonGBFont(16f);
+			PokeStart.setPokemonGBFont(14f);
 			ta = new TextArea(455,575,425,150,"Would you like to \ninteract with your\n" +pokemon + "?");
 		}
 		yes = new Button(560,640,50,50,"Yes",new Action() {
-			
+
 			@Override
 			public void act() {
-				Thread interact = new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						ta.setText("You spent a day playing\nwith "+ pokemon);
-						ta.update();
-						changeFatigue();
-						changeStats();
-						yes.setEnabled(false);
-						no.setEnabled(false);
-						PokeStart.inventory.daysLeft--;
-						try {
-							Thread.sleep(3000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+				if(PokeStart.inventory.daysLeft >= 1) {	
+					Thread interact = new Thread(new Runnable() {
+
+						@Override
+						public void run() {
+
+							if(pokemon.length() <=5) {
+								ta.setText("You spent a day\nplaying with "+ pokemon);
+							}
+							else ta.setText("You spend a day playing\nwith "+ pokemon);
+							ta.update();
+							changeFatigue();
+							changeStats();
+							yes.setEnabled(false);
+							no.setEnabled(false);
+							PokeStart.inventory.daysLeft--;
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							goBack();
 						}
-						goBack();
-					}
-				});
-				interact.start();
+					});
+					interact.start();
+				}
 			}
 		});
 		no = new Button(665,640,50,50,"No",new Action() {
-			
+
 			@Override
 			public void act() {
 				//lowkey a back button
@@ -94,17 +102,17 @@ public class RickyInteractScreen extends FullFunctionScreen {
 		viewObjects.add(yes);
 		viewObjects.add(no);
 	}
-	
+
 	public void changeFatigue() {
 		if(PokeStart.inventory.fatigue > 10) { 
 			PokeStart.inventory.fatigue -= 10;
 		}	
 		else PokeStart.inventory.fatigue = 0;
 	}
-	
+
 	public void changeStats() {
 		Pokemon pocketmonster = PokeStart.inventory.getPokemon();
-		
+
 		pocketmonster.setHp(PokeStart.inventory.getPokemon().getHp() + 1);
 		pocketmonster.setAtk(pocketmonster.getAtk() + 1);
 		pocketmonster.setDef(pocketmonster.getDef() + 1);
@@ -112,18 +120,18 @@ public class RickyInteractScreen extends FullFunctionScreen {
 		pocketmonster.setsDef(pocketmonster.getsDef() + 1);
 		pocketmonster.setSpd(pocketmonster.getSpd() + 1);
 	}	
-	
-//	public void addBackButton(List<Visible> viewObjects) {
-//		backButton = new Button(50,625,100,100, "Back", new Action() {
-//			@Override
-//			public void act() {
-//				PokeStart.start.setScreen(PokeStart.mainMenuScreen);
-//				PokeStart.mainScreen =! PokeStart.mainScreen;
-//			}
-//		});
-//		viewObjects.add(backButton);
-//	}
-	
+
+	//	public void addBackButton(List<Visible> viewObjects) {
+	//		backButton = new Button(50,625,100,100, "Back", new Action() {
+	//			@Override
+	//			public void act() {
+	//				PokeStart.start.setScreen(PokeStart.mainMenuScreen);
+	//				PokeStart.mainScreen =! PokeStart.mainScreen;
+	//			}
+	//		});
+	//		viewObjects.add(backButton);
+	//	}
+
 	public void goBack() {
 		PokeStart.mainMenuScreen = new MainMenuScreen(getWidth(), getHeight());
 		PokeStart.start.setScreen(PokeStart.mainMenuScreen);
