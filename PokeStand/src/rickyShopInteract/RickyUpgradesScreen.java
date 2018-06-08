9f1a3a46a2247bce981688b76d6d5efa9438c5f3
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-06-08 11:29:53 -0400

    Im working on it...

--- PokeStand/src/rickyShopInteract/RickyUpgradesScreen.java
+++ PokeStand/src/rickyShopInteract/RickyUpgradesScreen.java
@@ -1,12 +1,18 @@
 package rickyShopInteract;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import garrettPokemonTraining.Move;
+import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 import startGame.PokeStart;
 
 public class RickyUpgradesScreen extends FullFunctionScreen {
 
+	public ArrayList<Move> movesArray;
+	
 	public RickyUpgradesScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -15,9 +21,9 @@ public class RickyUpgradesScreen extends FullFunctionScreen {
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		// TODO Auto-generated method stub
-		int level = PokeStart.inventory.getPokemon().getLevel();
-
-		
+		movesArray = PokeStart.inventory.getPokemon().getMoves();
+		Graphic background = new Graphic(getHeight(),getWidth(),0,0,"resources/upgradeBackground.jpg");
+		viewObjects.add(background);
 	}
 
 }
